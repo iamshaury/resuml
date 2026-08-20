a65b1f1b30db14fb7d5a0fa09d1f8d6b5f3f4f3a
iamshaury <officialshaury@gmail.com>
2026-08-20 10:44:50 +0530

    chore: remove legacy database migration scripts, documentation, and utility tools

--- test-db.js
+++
@@ -1,7 +0,0 @@
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL + '/rest/v1/profiles?select=*&limit=1';
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-fetch(url, { headers: { 'apikey': key, 'Authorization': 'Bearer ' + key } })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
